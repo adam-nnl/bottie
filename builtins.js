2eafcc85bd6e8e9fee38e76e125d597b7126f035
Adam Lara <adam.nnl@gmail.com>
2016-09-08 22:59:20 -0500

    Update builtins.js

--- builtins.js
+++ builtins.js
@@ -17,10 +17,6 @@ module.exports = {
     'be a good bot and parrot benf fodjsi bdfsjdhf bazz back to me',
     'copy cat behf sdkjhfg peiurf fdlkgj kwjhe foufg'
   ],
-  rps: [
-    'rps',
-    'rps'
-  ],
   google: [
     'google',
     'Google'
