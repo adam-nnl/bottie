dcd88e2dfc2d7851dbfdd5cc84502a45af3f1a01
Adam Lara <adam.nnl@gmail.com>
2016-09-11 15:13:18 -0500

    Update empty.skill.js

--- src/empty.skill.js
+++ src/empty.skill.js
@@ -2,5 +2,5 @@
 module.exports = function(skill, info, bot, message) {
   console.log('INVOCATION OF NON-CONFIGURED SKILL: ' + skill);
   bot.reply(message, 'I understood this as ' + skill +
-    ', but you haven\'t configured how to make me work yet! Tell your local code monkey to fill in the code module for' + skill);
+    ', but you haven\'t configured how to make me work yet! Tell your local code monkey to fill in the code module for ' + skill);
 };
