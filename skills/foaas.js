d9b90ddce3bb30be405aa8a8472814b3fa918834
Adam Lara <adam.nnl@gmail.com>
2016-09-11 16:11:01 -0500

    Update foaas.js

--- skills/foaas.js
+++ skills/foaas.js
@@ -1,11 +1,5 @@
 
 module.exports = function(skill, info, bot, message) {
-  var request = require('request');
-  request('http://www.foaas.com/version', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      //console.log(body) // Show the HTML for the Google homepage.
-      bot.reply(message, body)
-    }
-  })
+  bot.reply(message, 'http://foaas.com/everyone/:' + bot.identity.name);
   //bot.reply(message, 'I understood this as ' + skill + ', but you haven\'t configured how to make me work yet! Tell your local code monkey to fill in the code module for ' + skill);
 };
