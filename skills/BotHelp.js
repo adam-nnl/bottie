d26832620a1ddc25911fe3d9a30ec34c0a0555c8
Adam Lara <adam.nnl@gmail.com>
2016-09-11 21:31:19 -0500

    Update BotHelp.js

--- skills/BotHelp.js
+++ skills/BotHelp.js
@@ -1,29 +1,4 @@
 
 module.exports = function(skill, info, bot, message) {
     bot.reply(message,'These are my current commands: ' );
-var builtinPhrases = require('./builtins');
-
-var customPhrasesText;
-var customPhrases;
-try {
-  customPhrasesText = fs.readFileSync(__dirname + '/custom-phrases.json').toString();
-} catch (err) {
-  throw new Error('Uh oh, Bottie could not find the ' +
-    'custom-phrases.json file, did you move it?');
-}
-try {
-  customPhrases = JSON.parse(customPhrasesText);
-} catch (err) {
-  throw new Error('Uh oh, custom-phrases.json was ' +
-    'not valid JSON! Fix it, please? :)');
-    
-    //bot.reply(message,'These are all my currently supported commands: ');
-    Object.keys(builtinPhrases).forEach(function(key) {
-    bot.reply(message, builtinPhrases[key]);
-    //callback(key, builtinPhrases[key]);
-  });
-    Object.keys(customPhrases).forEach(function(key) {
-    bot.reply(message, customPhrases[key]);
-    //callback(key, builtinPhrases[key]);
-  });    
 };
